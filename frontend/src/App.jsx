f03631145092977c78f693d1bb062cdbe25172e7
Anvit-13 <anvitfutane.01@gmail.com>
2026-05-02 14:14:55 +0530

    Complete My Bookings tab for customers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import SignupPage from './pages/auth/SignupPage';
 import CustomerDashboard from './pages/customer/CustomerDashboard';
 import BookingWizard from './pages/customer/BookingWizard';
 import BookingConfirmation from './pages/customer/BookingConfirmation';
+import MyBookings from './pages/customer/MyBookings';
 
 // Organiser Pages
 import AppointmentList from './pages/organiser/AppointmentList';
@@ -34,6 +35,7 @@ function App() {
         {/* Protected Customer Routes */}
         <Route element={<ProtectedRoute allowedRoles={['customer']} />}>
           <Route path="/dashboard" element={<CustomerDashboard />} />
+          <Route path="/bookings" element={<MyBookings />} />
           <Route path="/booking/:serviceId" element={<BookingWizard />} />
           <Route path="/confirmation" element={<BookingConfirmation />} />
         </Route>
